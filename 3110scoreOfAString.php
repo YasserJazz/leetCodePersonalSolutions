509fe013108c4a1bfd2ba121515fc66d30786a1a
Muhmmad Yasser Jazirahly <muhmmad.yas.jaz@gmail.com>
2024-08-03 01:24:57 +0400

    Final solution for 3110

--- 3110scoreOfAString.php
+++ 3110scoreOfAString.php
@@ -7,5 +7,13 @@ class Solution
      * @param String $s
      * @return Integer
      */
-    function scoreOfString($s) {}
+    function scoreOfString(string $s): int
+    {
+        $score = 0;
+        $stringAsArray = str_split($s);
+        for ($x = 0; $x < count($stringAsArray) - 1; $x++) {
+            $score += abs(ord($stringAsArray[$x]) - ord($stringAsArray[$x + 1]));
+        }
+        return $score;
+    }
 }
